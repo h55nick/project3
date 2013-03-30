b5788c29ed7d4872e77bfbde50d653165c5a91fe
Bryan W. Reid <bwreid@gmail.com>
2013-03-30 11:34:02 -0400

    main career exploration page created

--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,13 @@ Project3::Application.routes.draw do
       post '/answer' => 'users#answer_question'
     end
   end
-<<<<<<< HEAD
 
   resources :careers, :only => [:index, :show] do
     collection do
       get :zone_filter
-    end
-  end
-=======
-  resources :careers, :only => [:show] do
-    collection do
       get '/search_view' => 'careers#test_search'
       get '/search/' => 'careers#search_jobs'
     end
   end
 
->>>>>>> 789a20b0924d39eadd30239d593a842542ed0f3e
 end
