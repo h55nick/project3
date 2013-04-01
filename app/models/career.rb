26b26c6c39e3079d13120ebe645dee2082e0ec1d
Blanchet <h55nick@gmail.com>
2013-04-01 15:52:08 -0400

    seed file changes

--- app/models/career.rb
+++ app/models/career.rb
@@ -59,13 +59,17 @@ end
 
 
   def add_tasks
-    self.tasks.present? ? self.task.delete : ""
+    self.tasks.present? ? self.tasks = nil : ""
     url =  'http://www.onetonline.org/' + "link/table/details/tk/"+self.code+"/Tasks_"+self.code+".csv?fmt=csv&amp;s=IM&amp;t=-10"
-    tasks = HTTParty.get(url).split(/\r?\n/).map!{|d| d.split(",")[2..-1].join(",")}[1..3].join(" ").gsub(/\"/,'')
+    tasks = HTTParty.get(url)
+    tasks = tasks.split(/\r?\n/)
+    puts "+ Tasks "
+    tasks = tasks.map!{|d| d.split(",").length > 3 ? d.split(",")[2..-1].join(",") : ""}[1..3].join(" ").gsub(/\"/,'')
     self.tasks = tasks
     self.save
   end
   def add_interests
+    puts "+ Interests"
     self.interest.present? ? self.interest.delete : ""
     onet_base_url =  'http://www.onetonline.org/'
     intscv = "link/table/details/in/"+ self.code + "/Interests_"+self.code+".csv?fmt=csv&s=OI&t=-10"
@@ -76,6 +80,7 @@ end
     self.save #double check the save
   end
   def add_zone
+    puts "+ Zone"
     #career.zone.present? ? career.zone.delete : ""
     onet_base_url =  'http://www.onetonline.org/'
     zonscv = "link/table/details/jz/" + self.code + "/Job_Zone_"+ self.code+"47-5041-00.csv?fmt=csv"
@@ -85,6 +90,7 @@ end
     self.save
   end
   def add_trends
+    puts "+ Trends"
     onet_base_url =  'http://www.onetonline.org/'
     trend_url = onet_base_url + "link/details/"+ self.code
     s = Nokogiri::HTML(HTTParty.get(trend_url)).xpath('//table').last.text().split(/\r?\n/)
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,35 @@ def seed_stable_database()
     all_attrs = ["Social","Artistic","Enterprising","Conventional","Investigative","Realistic"]
     #Only need to Loop through each one idividually (permutations are included on the 1,2,3 IA (Interest Area))
     all_attrs.each do | a |
+      puts "#----------- Now on to " + a + "---------------#"
       attrcsv = "explore/interests-table/"+ a + '/'+a+".csv?fmt=csv"
       data_table_url = onet_base_url + attrcsv #creating full url
       table = HTTParty.get(data_table_url) #get csv
       rows = table.split(/\r?\n/).map!{|d| d.split(",")} #split by new line and then map each row into array based on , (comma) split
       rows[1..-1].each do |r| ##ADDING CAREERS
+        puts r[1]
         r.map!{|g| g.gsub(/\"/,'')}
-       c = Career.create(code:r[0],title:r[1..[(r.length-6),1].max].join(','),zone_num:r[-5])
-       c.add_interests
-       c.add_zone
-       c.add_trends
-       c.add_tasks #This is a property of Career but needs to be pulled from another csv table
+        c = Career.new(code:r[0],title:r[1..[(r.length-6),1].max].join(','),zone_num:r[-5])
+          if c.save
+            puts "Added Career- " + Career.all.length.to_s
+            puts Career.all[-1].title
+             c.add_interests
+             c.add_zone
+             c.add_trends
+             c.add_tasks
+          end #This is a property of Career but needs to be pulled from another csv table
       end
     end
 end
 
 #Comment this out if you want to keep the static database but reseed other
 ##### THIS WILL EFFECTIVELY WIPE THE "STATIC" DATABASE #####
-#  Career.delete_all
-#  Interest.delete_all
-#  Zone.delete_all
-#  Trend.delete_all
+  Career.delete_all
+  Interest.delete_all
+  Zone.delete_all
+  Trend.delete_all
 # # ## Create database
-#  seed_stable_database
+  seed_stable_database
 
 ### All "normal seeding" should go under this line: ###
 User.delete_all
