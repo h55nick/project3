db1095a4c459fb17a1bb4ea00bd8299b44fb9c06
Blanchet <h55nick@gmail.com>
2013-04-16 16:17:48 -0400

    linkedin oauth working v1.2

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,6 @@
 class SessionController < ApplicationController
   def new
   end
-
   def create
     user = User.where( email: params[:email] ).first
     if user.present? && user.authenticate( params[:password] )
