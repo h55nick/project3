c9c1f4a68bba981fd1fbb75631e11b9702da5059
Blanchet <h55nick@gmail.com>
2013-04-21 14:47:44 -0400

    linkedin play

--- app/controllers/linkedin_controller.rb
+++ app/controllers/linkedin_controller.rb
@@ -2,11 +2,13 @@ class LinkedinController < ApplicationController
 
   def index
     # get your api keys at https://www.linkedin.com/secure/developer
-    client = LinkedIn::Client.new(ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"])
+    linkedin_options ={  :request_token_path =>'/uas/oauth/requestToken?scope=r_fullprofile+r_emailaddress+r_contactinfo'}
+    client = LinkedIn::Client.new(ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"], linkedin_options)
 
     if session[:atoken]
       redirect_to oauth_callback_path
     else
+      puts "#------ LINKEDIN AUTH  (New User) -----#"
       request_token = client.request_token(:oauth_callback => "http://#{request.host_with_port}/oauth/callback")
       session[:rtoken] = request_token.token
       session[:rsecret] = request_token.secret
@@ -14,9 +16,11 @@ class LinkedinController < ApplicationController
     end
 
   end
-
+#:site => 'https://api.linkedin.com', :authorize_path => '/uas/oauth/authenticate',:access_token_path => '/uas/oauth/accessToken',
   def callback
-    client = LinkedIn::Client.new(ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"])
+    linkedin_options ={:request_token_path =>'/uas/oauth/requestToken?scope=r_fullprofile+r_emailaddress+r_contactinfo' }
+    #  linkedin_options = {}
+    client = LinkedIn::Client.new(ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"],linkedin_options)
     if session[:atoken].nil?
       pin = params[:oauth_verifier]
       atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
@@ -28,6 +32,7 @@ class LinkedinController < ApplicationController
 
     #All OAUTH DONE.
     @profile = client.profile
+    binding.pry
     #Find or create!
     user = User.find_or_create_by_l_token(l_token:session[:atoken],l_secret:session[:asecret],first:@profile[:first_name],last:@profile[:last_name],password:"a",password_confirmation:"a")
     user.interest ||= Interest.create
